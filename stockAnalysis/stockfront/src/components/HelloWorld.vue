862c9ec04adecf177d20a0b63bd315a75a557f7d
wayne <wayne.wang@blueplanet.com.tw>
2018-04-23 15:48:04 +0800

    add stockfront

---
+++ stockAnalysis/stockfront/src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">    
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+    <!-- <router-link to="/test">Go to test</router-link> -->
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Element from 'element-ui'
+Vue.use(Element)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
