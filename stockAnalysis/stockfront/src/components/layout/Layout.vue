d4d0b00f92b475eca36e82a9cb2fd4e1ede1025e
wayne <wayne.wang@blueplanet.com.tw>
2018-05-11 10:56:01 +0800

    modify migrations

--- stockAnalysis/stockfront/src/components/layout/Layout.vue
+++ stockAnalysis/stockfront/src/components/layout/Layout.vue
@@ -18,9 +18,6 @@
 export default {
   name: 'HelloWorld',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
   }
 }
 </script>
--- stockAnalysis/stockfront/src/router/index.js
+++ stockAnalysis/stockfront/src/router/index.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Layout from '@/components/layout/Layout'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.use(Router)
+Vue.use(ElementUI);
 
 export default new Router({
     routes: [{
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
+            component: Layout
         },
         {
             path: '/test',
-            name: 'HelloWorld',
             component: Test
         }
     ]
