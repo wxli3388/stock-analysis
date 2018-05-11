fc746dfca9c73e6f4f3a270b670a9e8e3e5f4c11
wayne <wayne.wang@blueplanet.com.tw>
2018-05-11 14:07:08 +0800

    line bot modfiy

--- stockAnalysis/stockWeb/ptt_data/stock_articles_notification.py
+++ stockAnalysis/stockWeb/ptt_data/stock_articles_notification.py
@@ -17,7 +17,8 @@ from stockWeb.models import Stock_articles
 
 try:
     author_white_list = ['lovdkkkk', 's10330076', 'MaInNine', 'q1w2e3r4t5']
-
+    #myline_id = 'U225d5f9dadad6cdfd1aa25a169228db2'
+    line_id = 'C4032f45274917ee678af67b882dfc2f6'        
 
     line_bot_api = LineBotApi('N8l3ZKewjdBmfplwDmh5j8K5uvaezW6J7Mpzv3wapa7v8dadamcZ/AKSXvADhiJaysBeINHzYCiDxmaz7eaZeQ2Cbrnpt1YIIS3Lfmm8Js+ZtfxzMKGc0RkWzVTDrX+Bf6wCmiCa098d3/XztWgyswdB04t89/1O/w1cDnyilFU=')
 
@@ -27,10 +28,9 @@ try:
     for row in rows:
         update_notification_id_list.append(row.sid)
 
-        #myline_id = 'U225d5f9dadad6cdfd1aa25a169228db2'
+        
         #line_bot_api.push_message(myline_id, TextSendMessage(text=row.title+'\n'+row.author+'\n'+datetime.strftime(row.publish_date, '%Y-%m-%d %H:%M:%S')+'\n'+row.article_url))
-
-        line_id = 'C4032f45274917ee678af67b882dfc2f6'        
+        
         line_bot_api.push_message(line_id, TextSendMessage(text=row.title+'\n'+row.author+'\n'+datetime.strftime(row.publish_date, '%Y-%m-%d %H:%M:%S')+'\n'+row.article_url))
         time.sleep(1)
     line_bot_api.push_message(line_id, TextSendMessage(text="卡比丟到雲端測試"))
